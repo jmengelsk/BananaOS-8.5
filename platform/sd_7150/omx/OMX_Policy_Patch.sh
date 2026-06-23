38b242aa53b98824c27ae5a6e51784b2c6b66fc2
mehedihjoy0 <mh506370@gmail.com>
2026-06-23 15:38:09 +0600

    platform/sd_7150: init

---
+++ platform/sd_7150/omx/OMX_Policy_Patch.sh
@@ -0,0 +1,16 @@
+# https://github.com/devcore94/MonsterROM/blob/sixteenQPR2/platform/exynos2100/patches/omx/customize.sh
+
+LOG_BEGIN "- Applying omx-patch"
+OMX_POLICY="$WORK_DIR/vendor/etc/seccomp_policy/samsung.software.media.c2-base-policy"
+
+if [ -f "$OMX_POLICY" ]; then
+    LOG "- Updating mremap rule in $OMX_POLICY"
+    # Replace the existing line if present
+    sed -i 's/^mremap: arg3 == 3$/mremap: arg3 == 3 || arg3 == MREMAP_MAYMOVE/' "$OMX_POLICY"
+
+    # If the line wasn't found, append it
+    if ! grep -q "mremap: arg3 == 3 || arg3 == MREMAP_MAYMOVE" "$OMX_POLICY"; then
+        echo "mremap: arg3 == 3 || arg3 == MREMAP_MAYMOVE" >> "$OMX_POLICY"
+    fi
+fi
+LOG_END
